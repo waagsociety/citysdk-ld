87cd57abef8b5b909a64c7c1d4371501ea09ed03
gitolite tester <tester@example.com>
2014-06-16 11:03:50 +0200

    test database setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'rack/test'
 require File.expand_path("../../config/environment", __FILE__)
 
 load 'spec/data/db_setup.rb'
+system 'cd db && ruby run_migrations.rb test'
 
 RSpec.configure do |config|
   config.mock_with :rspec
