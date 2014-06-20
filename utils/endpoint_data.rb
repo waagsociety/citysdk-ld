22ca3095e4d8ca8a98479ba26b72f7567a311372
Bert Spaan <bert.spaan@gmail.com>
2014-06-20 16:37:31 +0200

    removed unneeded variable

--- utils/endpoint_data.rb
+++ utils/endpoint_data.rb
@@ -2,7 +2,7 @@ module CitySDKLD
 
   def self.get_endpoint_data(query)
     config = CitySDKLD::App.get_config
-    data = {
+    {
       url: "http://#{query[:host]}",
       swagger: "http://#{query[:host]}/swagger",
     }.merge(config[:endpoint]).merge({
