c020e2cec8d0e8d2a3d7ef90c86311a2f3a1e834
Tom Demeyer <tom@waag.org>
2014-07-07 16:31:01 +0200

    start with ngsi10

--- app/api.rb
+++ app/api.rb
@@ -98,6 +98,8 @@ module CitySDKLD
     mount ::CitySDKLD::Endpoints
     mount ::CitySDKLD::Sessions
 
+    mount ::CitySDKLD::NGSI10
+
     # Notice: Swagger _must_ be called _after_ all endpoints are mounted
     # TODO: swagger should specify possible output formats (JSON, JSON-LD, Turtle, ...)
     add_swagger_documentation api_version: 'v1', 
