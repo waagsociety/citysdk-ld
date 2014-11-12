9815cd9007d4b878077374ad48b05b004c84b1f8
Bert Spaan <bert.spaan@gmail.com>
2014-11-12 10:49:42 +0100

    strings to symbols

--- app/ngsi_query.rb
+++ app/ngsi_query.rb
@@ -5,9 +5,12 @@ module CitySDKLD
   class NGSI10
 
     def self.do_query(query)
-      @limit   = query[:params][:limit] ? [1000,query[:params][:limit].to_i].min : 20
-      @offset  = query[:params][:offset] ? query[:params][:offset].to_i : 0
-      @details = (query[:params][:details] and query[:params][:details] == "on") ? true : false
+
+      puts query[:data].inspect
+
+      @limit = query[:params][:limit] ? [1000, query[:params][:limit].to_i].min : 20
+      @offset = query[:params][:offset] ? query[:params][:offset].to_i : 0
+      @details = (query[:params][:details] and query[:params][:details] == 'on') ? true : false
       case query[:method]
         when :post
           return self.post(query)
@@ -16,7 +19,7 @@ module CitySDKLD
         when :get
           return self.get(query)
       end
-      return { ngsiresult: "unkown command"}
+      return { ngsiresult: 'unkown command'}
     end
 
     def self.get(q)
@@ -24,48 +27,47 @@ module CitySDKLD
         when :contextEntityTypes
           return self.query_contextentity_types(q) if q[:params][:cetype]
         when :contextEntities
-          return self.query_one_entity(q)    if q[:params][:entity]
+          return self.query_one_entity(q) if q[:params][:entity]
         when :attributes
           return self.query_one_attribute(q) if q[:params][:entity] and q[:params][:attribute]
           return self.query_contextentity_types(q) if q[:params][:cetype] and q[:params][:attribute]
       end
-      return { ngsiresult: "unkown command"}
+      return {ngsiresult: 'unkown command'}
     end
 
     def self.put(q)
       if q[:path][-1] == :attributes and q[:params][:entity]
         return self.update_attributes_for_entity(q)
       end
-      return { ngsiresult: "unkown command"}
+      return {ngsiresult: 'unkown command'}
     end
 
     def self.post(q)
       case q[:path][-1]
         when :updateContext
           return self.updateContext(q)
-        when :queryContext
-          return self.queryContext(q)
+        when :query_context
+          return self.query_context(q)
         when :subscribeContext
-          return { ngsiresult: "not yet implemented: " + q[:path][-1]}
+          return { ngsiresult: 'not yet implemented: ' + q[:path][-1]}
         when :updateContextSubscription
-          return { ngsiresult: "not yet implemented: " + q[:path][-1]}
+          return { ngsiresult: 'not yet implemented: ' + q[:path][-1]}
         when :unsubscribeContext
-          return { ngsiresult: "not yet implemented: " + q[:path][-1]}
+          return { ngsiresult: 'not yet implemented: ' + q[:path][-1]}
       end
-      return { ngsiresult: "unkown command"}
+      return {ngsiresult: 'unkown command'}
     end
-
 
     def self.update_attributes_for_entity(q)
       data = q[:data]
       newdata = {}
       pattern = "(.*)\\.#{Regexp::quote(q[:params][:entity])}$"
-      object = CDKObject.where(cdk_id: Regexp.new(pattern,Regexp::IGNORECASE)).first
+      object = CDKObject.where(cdk_id: Regexp.new(pattern, Regexp::IGNORECASE)).first
       if object
         layer = CitySDKLD.memcached_get(CDKLayer.memcached_key(object.layer_id.to_s))
-        data['attributes'].each do |a|
-          newdata[a['name']] = a['value']
-          a['value'] = ""
+        data[:attributes].each do |a|
+          newdata[a[:name]] = a[:value]
+          a[:value] = ''
         end
         q = q.dup
         q[:params][:cdk_id] = object.cdk_id
@@ -73,30 +75,30 @@ module CitySDKLD
         q[:data] = newdata
         q[:method] = :patch
         CDKObjectDatum.execute_write(q)
-        data[:statusCode] = { code: "200", reasonPhrase: "OK"}
+        data[:statusCode] = {code: '200', reasonPhrase: 'OK'}
         return data
       else
-        return { errorCode: { code: "404", reasonPhrase: "No context elements found" } }
+        return { errorCode: {code: '404', reasonPhrase: 'No context elements found'}}
       end
     end
 
     def self.updateContext(query)
-      ct_response = {contextResponses: [], statusCode: {code: "200", reasonPhrase: "OK"}}
+      ct_response = {contextResponses: [], statusCode: {code: '200', reasonPhrase: 'OK'}}
       data = query[:data]
-      if data['updateAction'] =~ /delete/i
+      if data[:updateAction] =~ /delete/i
         # delete attributes or contextentities
       else
-        data['contextElements'].each do |ce|
-          layer = CDKLayer.where(rdf_type: 'orion:' + ce['type']).or(rdf_type: ce['type']).first
+        data[:contextElements].each do |ce|
+          layer = CDKLayer.where(rdf_type: 'orion:' + ce[:type]).or(rdf_type: ce[:type]).first
           if !layer
             layer = self.create_layer(ce, query)
             self.create_object(query,layer,ce)
           else
-            object = CDKObject.where(cdk_id: CitySDKLD.cdk_id_from_id(layer.name, ce['id'])).first
+            object = CDKObject.where(cdk_id: CitySDKLD.cdk_id_from_id(layer.name, ce[:id])).first
             if object
-              self.update_object(query,layer,ce,object)
+              self.update_object(query, layer, ce, object)
             else
-              self.create_object(query,layer,ce)
+              self.create_object(query, layer, ce)
             end
           end
           ct_response[:contextResponses] << ce
@@ -106,15 +108,15 @@ module CitySDKLD
     end
 
     def self.query_one_entity(q)
-      @fieldTypes = []
-      r = get_one_entity({'id' => q[:params][:entity]},nil,nil)
-      (r and r[0]) ? r[0] : { errorCode: { code: "404", reasonPhrase: "No context elements found" } }
+      @field_types = []
+      r = get_one_entity({id: q[:params][:entity]}, nil, nil)
+      (r and r[0]) ? r[0] : {errorCode: {code: 404, reasonPhrase: 'No context elements found'}}
     end
 
     def self.query_one_attribute(q)
-      @fieldTypes = []
-      r = get_one_entity({'id' => q[:params][:entity]},nil,nil)
-      return { errorCode: { code: "404", reasonPhrase: "No context elements found" } } if r.nil?
+      @field_types = []
+      r = get_one_entity({id: q[:params][:entity]}, nil, nil)
+      return { errorCode: {code: 404, reasonPhrase: 'No context elements found'} } if r.nil?
       if r[0]
         r[0][:contextElement][:attributes].each do |a|
           if a[:name] == q[:params][:attribute]
@@ -129,18 +131,17 @@ module CitySDKLD
 
     def self.get_one_entity(ce, attributes, restriction)
       retvalue = []
-      if (ce['isPattern'] == true) or (ce['isPattern'] == "true")
-        pattern = "(.*)\\." + ce['id']
+      if ce[:isPattern] == true
+        pattern = "(.*)\\." + ce[:id]
         objects = self.objects_select_filter(CDKObject.where(cdk_id: Regexp.new(pattern,Regexp::IGNORECASE)), restriction)
-
-        @count = CDKObject.where(cdk_id: Regexp.new(pattern,Regexp::IGNORECASE)).count() if @details
+        @count = CDKObject.where(cdk_id: Regexp.new(pattern, Regexp::IGNORECASE)).count() if @details
         objects.each do |o|
           layer = CitySDKLD.memcached_get(CDKLayer.memcached_key(o.layer_id.to_s))
           self.populate_field_types(layer)
           retvalue << self.get_one_object(ce, o, layer, attributes)
         end
       else
-        object = self.objects_select_filter( CDKObject.where(Sequel.like(:cdk_id, "%#{ce['id'].downcase}")), restriction).first
+        object = self.objects_select_filter( CDKObject.where(Sequel.like(:cdk_id, "%#{ce[:id].downcase}")), restriction).first
         if object
           layer = CitySDKLD.memcached_get(CDKLayer.memcached_key(object.layer_id.to_s))
           self.populate_field_types(layer)
@@ -152,15 +153,15 @@ module CitySDKLD
 
     def self.get_one_layered_entity(ce, layer, attributes, restriction)
       retvalue = []
-      if (ce['isPattern'] == true) or (ce['isPattern'] == "true")
-        pattern = Regexp::quote(layer.name + ".") + ce['id']
+      if ce[:isPattern] == true
+        pattern = Regexp::quote(layer.name + ".") + ce[:id]
         objects = self.objects_select_filter(CDKObject.where(layer_id: layer.id, cdk_id: Regexp.new(pattern,Regexp::IGNORECASE)), restriction)
         @count  = CDKObject.where(layer_id: layer.id, cdk_id: Regexp.new(pattern,Regexp::IGNORECASE)).count() if @details
         objects.each do |o|
           retvalue << self.get_one_object(ce, o, layer, attributes)
         end
       else
-        cdk_id = CitySDKLD.cdk_id_from_id(layer.name, ce['id'])
+        cdk_id = CitySDKLD.cdk_id_from_id(layer.name, ce[:id])
         object = self.objects_select_filter(CDKObject.where(cdk_id: cdk_id), restriction).first
         if object
           retvalue << self.get_one_object(ce, object, layer, attributes)
@@ -170,14 +171,14 @@ module CitySDKLD
     end
 
     def self.get_one_object(ce, object, layer, attributes)
-      elm = {contextElement: {attributes: [], id: object[:title], isPattern: false, type: ce['type'] }, statusCode: {code: "200", reasonPhrase: "OK"} }
+      elm = {contextElement: {attributes: [], id: object[:title], isPattern: false, type: ce[:type] }, statusCode: {code: "200", reasonPhrase: "OK"} }
       odatum = CDKObjectDatum.get_from_object_and_layer(object.cdk_id, layer[:id])
       odatum[:data].each do |k,v|
         begin
           v = JSON.parse(v, symbolize_names: true)
         rescue
         end
-        elm[:contextElement][:attributes] << { name: k, value: v, type: @fieldTypes[layer[:id]][k] || 'unknown'} if attributes.blank? or attributes.include?(k)
+        elm[:contextElement][:attributes] << { name: k, value: v, type: @field_types[layer[:id]][k] || 'unknown'} if attributes.blank? or attributes.include?(k)
       end
       if object[:centr] =~ /POINT\(([\d\.]+)\s([\d\.]+)\)/
         if attributes.blank?
@@ -200,7 +201,7 @@ module CitySDKLD
 
     def self.query_contextentity_types(q)
       layer = @count = nil
-      @fieldTypes = []
+      @field_types = []
       cetype = q[:params][:cetype]
       attrs = q[:params][:attribute] ? [ q[:params][:attribute] ] : nil
       ct_response = {contextResponses: []}
@@ -210,7 +211,7 @@ module CitySDKLD
         objects = self.objects_select_filter(CDKObject.where(layer_id: layer.id), nil)
         @count  = CDKObject.where(layer_id: layer.id).count() if @details
         objects.each do |o|
-          ctResponse[:contextResponses] << self.get_one_object({'type' => cetype}, o, layer, attrs)
+          ct_response[:contextResponses] << self.get_one_object({'type' => cetype}, o, layer, attrs)
         end
       end
       return { errorCode: { code: "404", reasonPhrase: "No context elements found" } } if ct_response[:contextResponses].length == 0
@@ -218,56 +219,67 @@ module CitySDKLD
       return ct_response
     end
 
-    def self.queryContext(query)
+    def self.query_context(query)
       layer = @count = nil
-      @fieldTypes = []
+      @field_types = []
       ct_response = {contextResponses: []}
       data = query[:data]
-      attributes = data['attributes']
-      data['entities'].each do |ce|
-        if ce['type']
-          layer = CDKLayer.where(rdf_type: 'orion:' + ce['type']).or(rdf_type: ce['type']).first
+      attributes = data[:attributes]
+      data[:entities].each do |ce|
+        if ce[:type]
+          layer = CDKLayer.where(rdf_type: 'orion:' + ce[:type]).or(rdf_type: ce[:type]).first
           if layer
             self.populate_field_types(layer)
-            ct_response[:contextResponses] += self.get_one_layered_entity(ce, layer, attributes, data['restriction'])
+            ct_response[:contextResponses] += self.get_one_layered_entity(ce, layer, attributes, data[:restriction])
           end
         else
           # typeless query
-          ctResponse[:contextResponses] += self.get_one_entity(ce, attributes, data['restriction'])
+          ct_response[:contextResponses] += self.get_one_entity(ce, attributes, data[:restriction])
         end
       end
-      return { errorCode: { code: "404", reasonPhrase: "No context elements found" } } if ct_response[:contextResponses].length == 0
-      ct_response[:errorCode] = {code: 200, reasonPhrase: "OK", details: "Count: #{@count}" } if @count
+      return { errorCode: { code: '404', reasonPhrase: 'No context elements found' } } if ct_response[:contextResponses].length == 0
+      ct_response[:errorCode] = {code: 200, reasonPhrase: 'OK', details: "Count: #{@count}" } if @count
       return ct_response
     end
 
     def self.populate_field_types(l)
       # cache field types to reduce db queries
-      return if @fieldTypes[l[:id] || l['id']]
+      return if @field_types[l[:id] || l[:id]]
       layer = CitySDKLD.memcached_get(CDKLayer.memcached_key(l[:id].to_s))
-      @fieldTypes[l[:id]] = {}
+      @field_types[l[:id]] = {}
       layer[:fields].each { |f|
-        @fieldTypes[l[:id]][f[:name]] = f[:type]
+        @field_types[l[:id]][f[:name]] = f[:type]
       }
     end
 
     def self.create_object(query,layer,data)
-      object = { "type" => "Feature", "properties" => { "id" => data['id'], "title" => data['id'], "data" => { } },
-                 "geometry" => { "type" => "Point", "coordinates" => [4.90033, 52.37277] }
-               }
-      data['attributes'].each do |a|
-        if a['metadatas']
+      object = {
+        type: 'Feature',
+        properties: {
+          id: data[:id],
+          title: data[:id],
+          data: {}
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: []
+        }
+      }
+      data[:attributes].each do |a|
+        if a[:metadatas]
           seen_geometry = false
-          a['metadatas'].each do |m|
-            if m['name'] == 'location' and a['value'] =~ /([\d\.]+)[\s,]+([\d\.]+)/
-              object["geometry"]["coordinates"] = [$2.to_f,$1.to_f]
+          a[:metadatas].each do |m|
+            if m[:name] == 'location' and a[:value] =~ /([\d\.]+)[\s,]+([\d\.]+)/
+              object[:geometry][:coordinates] = [$2.to_f,$1.to_f]
               seen_geometry = true
             end
           end
           next if seen_geometry
         end
-        object["properties"]["data"][a['name']] = a['value']
-        a['value'] = "" # empty values for response object
+        object[:properties][:data][a[:name]] = a[:value]
+
+        # empty values for response object
+        a[:value] = ''
       end
 
       q = query.dup
@@ -277,11 +289,11 @@ module CitySDKLD
       CDKObject.execute_write(q)
     end
 
-    def self.update_object(query,layer,data,object)
+    def self.update_object(query, layer, data, object)
       newdata = {}
-      data['attributes'].each do |a|
-        newdata[a['name']] = a['value']
-        a['value'] = ""
+      data[:attributes].each do |a|
+        newdata[a[:name]] = a[:value]
+        a[:value] = ''
       end
       q = query.dup
       q[:params][:cdk_id] = object.cdk_id
@@ -293,37 +305,37 @@ module CitySDKLD
 
     def self.create_layer(data, query)
       layer = {
-        name: 'ngsi.' + data['type'].downcase,
-        title: data['type'] + " orion NGSI layer",
-        rdf_type: 'orion:' + data['type'],
+        name: 'ngsi.' + data[:type].downcase,
+        title: data[:type] + ' orion NGSI layer',
+        rdf_type: 'orion:' + data[:type],
         fields: [],
-        owner: "citysdk",
-        description: "System-generated, Fi-Ware Orion compatible data layer",
-        data_sources: ["NGSI"],
-        category: "none",
-        subcategory: "",
-        licence: "unspecified"
+        owner: 'citysdk',
+        description: 'System-generated, Fi-Ware Orion compatible data layer',
+        data_sources: ['NGSI'],
+        category: 'none',
+        subcategory: '',
+        licence: 'unspecified'
       }
-      data['attributes'].each do |a|
+      data[:attributes].each do |a|
         layer[:fields] << {
-          name: a['name'],
-          type: a['type'],
-          description: ""
+          name: a[:name],
+          type: a[:type],
+          description: ''
         }
       end
       q = query.dup
       q[:data] = layer
       q[:method] = :post
       CDKLayer.execute_write(q)
-      CDKLayer.where(rdf_type: 'orion:' + data['type']).first
+      CDKLayer.where(rdf_type: 'orion:' + data[:type]).first
     end
 
     def self.polygon(vertices)
       ret = ''
-      vertices.each do |v|
+      vertices.each do |vertex|
         ret << "," if ret.length > 0
-        ret << v["longitude"]
-        ret << " " + v["latitude"]
+        ret << vertex[:longitude]
+        ret << " " + vertex[:latitude]
       end
       'POLYGON((' + ret + '))'
     end
@@ -331,11 +343,11 @@ module CitySDKLD
     def self.objects_select_filter(dataset, restriction)
       dataset = dataset.select(:cdk_id, :layer_id, :title, Sequel.as(Sequel.function(:ST_AsText, Sequel.function(:ST_Centroid, :geom)), :centr))
       if restriction
-        restriction['scopes'].each do |s|
-          if s["type"] == "FIWARE_Location"
-            if s["value"]["polygon"] # p["vertices"], p["inverted"]
-              p = self.polygon(s["value"]["polygon"]["vertices"])
-              if s["value"]["polygon"]["inverted"] == true
+        restriction[:scopes].each do |s|
+          if s[:type] == "FIWARE_Location"
+            if s[:value][:polygon] # p["vertices"], p["inverted"]
+              p = self.polygon(s[:value][:polygon][:vertices])
+              if s[:value][:polygon][:inverted] == true
                 dataset = dataset.exclude(
                   Sequel.function(:ST_Contains,
                     Sequel.function(:ST_SetSRID,Sequel.function(:ST_PolygonFromText,p), 4326),
@@ -346,12 +358,12 @@ module CitySDKLD
                     Sequel.function(:ST_SetSRID,Sequel.function(:ST_PolygonFromText,p), 4326),
                       Sequel.function(:ST_Centroid, :geom)) )
               end
-            elsif s["value"]["circle"] # c["centerLatitude"], c["centerLongitude"], c["radius"], c["inverted"]
-              lat = s["value"]["circle"]["centerLatitude"].to_f
-              lon = s["value"]["circle"]["centerLongitude"].to_f
-              rad = s["value"]["circle"]["radius"].to_f
+            elsif s[:value][:circle] # c["centerLatitude"], c["centerLongitude"], c["radius"], c["inverted"]
+              lat = s[:value][:circle][:centerLatitude].to_f
+              lon = s[:value][:circle][:centerLongitude].to_f
+              rad = s[:value][:circle][:radius].to_f
               intersects = "ST_Intersects(ST_Transform(Geometry(ST_Buffer(Geography(ST_Transform(ST_SetSRID(ST_Point(#{lon}, #{lat}), 4326), 4326)), #{rad})), 4326), ST_Centroid(geom))"
-              if s["value"]["circle"]["inverted"] == true
+              if s[:value][:circle][:inverted] == true
                 dataset = dataset.exclude( intersects )
               else
                 dataset = dataset.where( intersects )
@@ -365,4 +377,3 @@ module CitySDKLD
   end
 
 end
-
