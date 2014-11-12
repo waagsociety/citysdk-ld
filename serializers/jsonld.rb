82dbe73b7ce2425080e301e4a01f7461fce6ab90
Bert Spaan <bert.spaan@gmail.com>
2014-11-12 14:27:31 +0100

    added TODOs

--- serializers/jsonld.rb
+++ serializers/jsonld.rb
@@ -97,37 +97,109 @@ module CitySDKLD
         end
       end
 
+      # def layer_on_object
+      #   # TODO: use
+      #   #:imported_at => 'dcat:modified'
+      # end
+
       def create_object_context
         endpoint_data = CitySDKLD.get_endpoint_data(@query)
+
         {
-          :@base => endpoint_data[:base_uri],
-          :title => "dc:title",
-          :cdk_id => ":cdk_id",
-          :features => ":apiResult",
-          :properties => "_:properties",
-          :date_created => "dc:date",
-          :layer => {
-            :@id => ":createdOnLayer",
-            :@type => "@id"
+          :@vocab => endpoint_data[:base_uri],
+        }.merge(CitySDKLD::PREFIXES).merge({
+          title: 'dc:title',
+          cdk_id: ':cdk_id',
+          features: ':apiResult',
+          properties: '_:properties',
+          date_created: 'dc:date',
+          layer: {
+            :@id => ':createdOnLayer',
+            :@type => '@id'
           },
-          :layers => {
-            :@id => ":layerOnObject",
-            :@container => "@index"
+          layers: {
+            :@id => ':layerOnObject',
+            :@container => '@index'
           },
-          :data => ":layerData"
-          #:wkt => "geos:hasGeometry"
-        }
+          data: ':layerData',
+          geometry: nil,
+          type: nil
+        })
       end
 
       def create_layer_context
         endpoint_data = CitySDKLD.get_endpoint_data(@query)
+
         {
-          :@base => endpoint_data[:base_uri],
-          :title => 'dct:title',
-          :features => ':apiResult',
-          :properties => '_:properties',
-          :imported_at => 'dct:modified'
-        }
+          :@vocab => endpoint_data[:base_uri],
+        }.merge(CitySDKLD::PREFIXES).merge({
+          title: 'dcat:title',
+          features: ':apiResult',
+          properties: '_:properties',
+          name: 'rdfs:label',
+          title: 'dc:title',
+          description: 'dc:description',
+          category: {
+
+          },
+          subcategory: ':subcategory',
+          geometry: nil,
+          type: nil,
+
+
+          # # rdf_type: {
+          # #   :'@id' => '',
+          # #   :'@type'=> '@id'
+          # # }
+          # licence
+          # data_sources
+          # fields
+          # owner
+        })
+
+
+
+        # @layers.each do |l, layer|
+        #   layer[:fields].each do |field|
+        #     @result << "<layers/#{l}/fields/#{field[:id]}>"
+        #     @result << "    :definedOnLayer <layers/#{l}> ;"
+        #
+        #     @result << "    rdf:type #{field[:type]} ;" if field[:type]
+        #     @result << "    rdfs:description #{field[:description].to_json} ;" if field[:description]
+        #     @result << "    xsd:language #{field[:lang].to_json} ;" if field[:lang]
+        #     @result << "    owl:equivalentProperty #{field[:eqprop]} ;" if field[:eqprop]
+        #     @result << "    :hasValueUnit #{field[:unit]} ;" if field[:unit]
+        #
+        #     @result << "    rdfs:subPropertyOf :layerProperty ."
+        #     @result << ""
+        #   end
+        # end
+
+
+        #
+        # "licence": "http://www.openstreetmap.org/copyright",
+        # "data_sources": [
+        #
+        #     "http://www.openstreetmap.org/"
+        #
+        # ],
+        #
+        # # NieT: "rdf_prefixes"
+        #
+        # "owner": {
+        #
+        #     "name": "citysdk",
+        #     "fullname": "CitySDK",
+        #     "email": "citysdk@waag.org",
+        #     "organization": "CitySDK LD",
+        #     "admin": true
+        #
+        # },
+        # "fields": [
+
+
+        #TODO: toevoegen prefixes
+        #TODO: TYPE van het dataojbject!!!!!
       end
 
     end
