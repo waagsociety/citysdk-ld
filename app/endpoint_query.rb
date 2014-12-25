05ab3a634af62410ae2ef7a1bc609b422278caec
Tom Demeyer <tom@waag.org>
2014-12-25 14:15:45 +0100

    fix character encoding

--- app/endpoint_query.rb
+++ app/endpoint_query.rb
@@ -2,6 +2,7 @@
 
 class Grape::Endpoint
   def do_query(resource, single = false)
+    header 'Content-Type', 'application/json; charset=utf-8'
     q = CitySDKLD::Query.new resource, single, env
     q.execute
   end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,10 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
+#
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +15,8 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '195.169.149.30', user: 'citysdk', roles: %w{web app}
+# server '195.169.149.21', user: 'citysdk', roles: %w{db}
 
 
 # Custom SSH Options
